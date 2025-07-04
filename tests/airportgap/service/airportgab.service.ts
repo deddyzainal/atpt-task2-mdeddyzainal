2e9a454699af491a4e109b1518a4ab040f3da134
M Deddy Zainal <mdeddyz@gmail.com>
2025-07-04 11:26:41 +0700

    [Deddy] update service class

--- tests/airportgap/service/airportgab.service.ts
+++ tests/airportgap/service/airportgab.service.ts
@@ -1,94 +1,170 @@
-import { APIRequestContext } from '@playwright/test';
+import { APIRequestContext, request } from '@playwright/test';
 
 const BASE_URL = 'https://airportgap.com/api';
 
-export async function getAirports(context: APIRequestContext) {
-  return context.get(`${BASE_URL}/airports`);
+export class AirportGabService {
+  async getAirports() {
+    try {
+      const context = await request.newContext();
+      const response = await context.get(`${BASE_URL}/airports`);
+
+      const data = await response.json();
+      const status = response.status();
+
+      return {
+        data: data,
+        status: status
+      };
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  async getAirportById(id: string) {
+    try {
+      const context = await request.newContext();
+      const response = await context.get(`${BASE_URL}/airports/${id}`);
+
+      const data = await response.json();
+      const status = response.status();
+
+      return { data, status };
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  async getDistance(from: string, to: string) {
+    try {
+      const context = await request.newContext();
+      const response = await context.post(`${BASE_URL}/airports/distance`, {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        form: {
+          from,
+          to,
+        },
+      });
+
+      const data = await response.json();
+      const status = response.status();
+
+      return { data, status };
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  async getFavorites(token: string) {
+    try {
+      const context = await request.newContext();
+      const response = await context.get(`${BASE_URL}/favorites`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      const data = await response.json();
+      const status = response.status();
+
+      return { data, status };
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  async getFavoriteById(token: string, favoriteId: number) {
+    try {
+      const context = await request.newContext();
+      const response = await context.get(`${BASE_URL}/favorites/${favoriteId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      const data = await response.json();
+      const status = response.status();
+
+      return { data, status };
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  async addFavorite(token: string, airportId: string) {
+    try {
+      const context = await request.newContext();
+      const response = await context.post(`${BASE_URL}/favorites`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        form: {
+          airport_id: airportId,
+        },
+      });
+
+      const data = await response.json();
+      const status = response.status();
+
+      return { data, status };
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  async patchFavorite(token: string, favoriteId: number, note: string) {
+    try {
+      const context = await request.newContext();
+      const response = await context.patch(`${BASE_URL}/favorites/${favoriteId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        form: {
+          note: note,
+        },
+      });
+
+      const data = await response.json();
+      const status = response.status();
+
+      return { data, status };
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  async deleteFavorite(token: string, favoriteId: number) {
+    try {
+      const context = await request.newContext();
+      const response = await context.delete(`${BASE_URL}/favorites/${favoriteId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      const status = response.status();
+      return { status };
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  async deleteAllFavorite(token: string) {
+    try {
+      const context = await request.newContext();
+      const response = await context.delete(`${BASE_URL}/favorites/clear_all`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      const status = response.status();
+      return { status };
+    } catch (e) {
+      throw e;
+    }
+  }
 }
-
-export async function getAirportById(context: APIRequestContext, id: string) {
-  return context.get(`${BASE_URL}/airports/${id}`);
-}
-
-export async function getDistance(context: APIRequestContext, from: string, to: string) {
-  return context.post(`${BASE_URL}/airports/distance`, {
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    form: {
-      from,
-      to,
-    },
-  });
-}
-
-export async function getFavorites(context: APIRequestContext, token: string) {
-  const response = await context.get(`${BASE_URL}/favorites`, {
-    headers: { Authorization: `Bearer ${token}` },
-  });
-  return response;
-}
-
-export async function getFavoriteById(context: APIRequestContext, token: string, favoriteId: number) {
-  const url = `${BASE_URL}/favorites/${favoriteId}`;
-
-  const response = await context.get(url, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  return response;
-}
-
-
-export async function addFavorite(context: APIRequestContext, token: string, airportId: string) {
-  const url = `${BASE_URL}/favorites`;
-  const headers = {
-    Authorization: `Bearer ${token}`,
-    'Content-Type': 'application/x-www-form-urlencoded'
-  };
-  const form = { airport_id: airportId };
-
-  return context.post(url, {
-    headers,
-    form
-  });
-}
-
-export async function patchFavorite(context: APIRequestContext, token: string, favoriteId: number, note: string) {
-  const url = `${BASE_URL}/favorites/${favoriteId}`;
-  const headers = {
-    Authorization: `Bearer ${token}`,
-    'Content-Type': 'application/x-www-form-urlencoded'
-  };
-  const form = { note: note };
-
-  return context.patch(url, {
-    headers,
-    form
-  });
-}
-
-
-export async function deleteFavorite(context: APIRequestContext, token: string, favoriteId: number) {
-  const url = `${BASE_URL}/favorites/${favoriteId}`;
-  const headers = {
-    Authorization: `Bearer ${token}`
-  };
-
-  return context.delete(url, {
-    headers
-  });
-}
-
-export async function deleteAllFavorite(context: APIRequestContext, token: string) {
-  const url = `${BASE_URL}/favorites/clear_all`;
-  const headers = {
-    Authorization: `Bearer ${token}`
-  };
-
-  return context.delete(url, {
-    headers
-  });
-}
-
--- tests/airportgap/spec/airportgab.spec.ts
+++ tests/airportgap/spec/airportgab.spec.ts
@@ -1,101 +1,82 @@
-import { test, expect, APIRequestContext } from '@playwright/test';
-import {
-  getAirports,
-  getAirportById,
-  getDistance,
-  getFavorites,
-  addFavorite,
-  patchFavorite,
-  deleteFavorite,
-  getFavoriteById,
-  deleteAllFavorite,
-} from '../service/airportgab.service';
+import { test, expect } from '@playwright/test';
+import { AirportGabService } from '../service/airportgab.service';
 import { createAuthContext } from '../context/api.context';
 
 test.describe('Airport Gap API Tests', () => {
-    test('GET /airports', async ({ request }) => {
-        const response = await getAirports(request);
-        expect(response.ok()).toBeTruthy();
-        const data = await response.json();
-        expect(Array.isArray(data.data)).toBeTruthy();
-    });
+  const getAirportService = new AirportGabService();
 
-    test('GET /airports/{id}', async ({ request }) => {
-        const response = await getAirportById(request, 'GKA');
-        expect(response.ok()).toBeTruthy();
-        const data = await response.json();
-        expect(data.data.id).toBe('GKA');
-    });
-    
-    test('POST /airports/distance', async ({ request }) => {
-        const response = await getDistance(request, 'KIX', 'NRT');
-        expect(response.ok()).toBeTruthy();
-        const data = await response.json();
-        expect(data.data.attributes.kilometers).toBeGreaterThan(0);
-        });
+  test('GET /airports', async () => {
+    const { data, status } = await getAirportService.getAirports();
+    expect(data).toBeTruthy();
+    expect(status).toBe(200);
+  });
 
-    test('POST /tokens', async () => {
-        const { token } = await createAuthContext();
+  test('GET /airports/{id}', async () => {
+    const { data, status } = await getAirportService.getAirportById('GKA');
+    expect(data).toBeTruthy();
+    expect(status).toBe(200);
+    expect(data.data.id).toBe('GKA');
+  });
 
-        expect(token).toBeDefined();
-        expect(typeof token).toBe('string');
-        expect(token.length).toBeGreaterThan(10);
-    });
+  test('POST /airports/distance', async () => {
+    const { data, status } = await getAirportService.getDistance('KIX', 'NRT');
+    expect(data).toBeTruthy();
+    expect(status).toBe(200);
+    expect(data.data.attributes.kilometers).toBeGreaterThan(0);
+  });
+
+  test('POST /tokens', async () => {
+    const { token } = await createAuthContext();
+    expect(token).toBeDefined();
+    expect(typeof token).toBe('string');
+    expect(token.length).toBeGreaterThan(10);
+  });
 
   test.describe.serial('Authorized requests', () => {
     let token: string;
     let favoriteId: number;
-    let context: APIRequestContext;
 
     test.beforeAll(async () => {
       const auth = await createAuthContext();
       token = auth.token;
-      context = auth.context;
-      await deleteAllFavorite(context, token);
+      await getAirportService.deleteAllFavorite(token);
     });
 
-    test('POST /favorites', async () => {  
-      const response = await addFavorite(context, token, 'GKA');      
-      const data = await response.json();
-      expect(response.status()).toBe(201);
-      expect(response.ok()).toBeTruthy();
-      
+    test('POST /favorites', async () => {
+      const { data, status } = await getAirportService.addFavorite(token, 'GKA');
+      expect(status).toBe(201);
+      expect(data).toBeTruthy();
+
       favoriteId = data.data.id;
       expect(data.data.attributes.airport.iata).toEqual('GKA');
     });
 
-    test('PATCH /favorites', async () => {  
-      const response = await patchFavorite(context, token, favoriteId, 'Test note');      
-      const data = await response.json();
-      expect(response.status()).toBe(200);
-      expect(response.ok()).toBeTruthy();
-      
+    test('PATCH /favorites', async () => {
+      const { data, status } = await getAirportService.patchFavorite(token, favoriteId, 'Test note');
+      expect(status).toBe(200);
       expect(data.data.attributes.note).toEqual('Test note');
     });
 
     test('GET /favorites', async () => {
-      const response = await getFavorites(context, token);
-      expect(response.status()).toBe(200);
-      expect(response.ok()).toBeTruthy();
+      const { data, status } = await getAirportService.getFavorites(token);
+      expect(status).toBe(200);
+      expect(data).toBeTruthy();
     });
 
     test('GET /favorites/{id}', async () => {
-        const response = await getFavoriteById(context, token, favoriteId);
-        const data = await response.json();
-        expect(response.status()).toBe(200);
-        expect(response.ok()).toBeTruthy();
-
-        expect(data.data.attributes.airport.iata).toEqual('GKA');
+      const { data, status } = await getAirportService.getFavoriteById(token, favoriteId);
+      expect(status).toBe(200);
+      expect(data.data.attributes.airport.iata).toEqual('GKA');
     });
 
     test('DELETE /favorites/{id}', async () => {
-      const response = await deleteFavorite(context, token, favoriteId);
-      expect(response.status()).toBe(204);
+      const { status } = await getAirportService.deleteFavorite(token, favoriteId);
+      expect(status).toBe(204);
     });
 
     test('DELETE /favorites/clear_all', async () => {
-      const response = await deleteAllFavorite(context, token);
-      expect(response.status()).toBe(204);
+      const { status } = await getAirportService.deleteAllFavorite(token);
+      expect(status).toBe(204);
     });
   });
 });
